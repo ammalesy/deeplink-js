4ca66158e664a10cac500f5b1dd0b33bf352682b
Ammales Yamsompong <ammales.y@kbtg.tech>
2025-11-08 22:37:48 +0700

    Support URLScheme Solution

--- kplus-deeplink.js
+++ kplus-deeplink.js
@@ -5,10 +5,11 @@
  */
 
 function KPlusDeepLinkHandler() {
-  this.huaweiUrl = "https://kplusuat.dra.agconnect.link/Dtest";
+  this.huaweiUrl = "https://kplusuat.dra.agconnect.link/?deeplink=https%3A%2F%2Fwww.kasikornbank.com%2Fth%2Fkplus%2Fdeeplinkkplus&android_deeplink=kbank.kplus%3A%2F%2Fauthenwithkplus%3FnextAction%3DNEXT_ACTION_REPLACEMENT%26tokenId%3DTOKEN_ID_REPLACEMENT&android_fallback_url=https%3A%2F%2Fwww.kasikornbank.com%2Fth%2Fkplus%2Fdeeplinkkplus&android_open_type=3&android_package_name=com.kasikorn.retail.mbanking.wap2&campaign_channel=First+Test+HMS&harmonyos_deeplink=kbank.kplus%3A%2F%2Fauthenwithkplus&ios_link=kbank.kplus%3A%2F%2Fauthenwithkplus&preview_type=2&landing_page_type=2&region_id=3"
+  //"https://kplusuat.dra.agconnect.link/Dtest";
   this.generalUrl = "https://www.kasikornbank.com/th/kplus/deeplinkkplus/";
   this.fallbackUrl = "https://www.kasikornbank.com/th/kplus/deeplinkkplus/";
-  this.deepLinkEnabled = true; // Flag to enable/disable deep link functionality
+  this.deepLinkEnabled = false; // Flag to enable/disable deep link functionality
 }
 
 /**
@@ -76,16 +77,17 @@ KPlusDeepLinkHandler.prototype.openKPlusApp = function(token, nextAction) {
   var fullUrl;
   
   if (this.isHuaweiDevice()) {
-    baseUrl = this.huaweiUrl;
+    // For Huawei devices, replace placeholders in the URL
+    fullUrl = this.huaweiUrl
+      .replace('NEXT_ACTION_REPLACEMENT', encodeURIComponent(nextAction))
+      .replace('TOKEN_ID_REPLACEMENT', encodeURIComponent(token));
   } else {
     baseUrl = this.generalUrl;
+    // Check if URL already has query parameters
+    var separator = baseUrl.indexOf('?') !== -1 ? '&' : '?';
+    // Build full URL with parameters
+    fullUrl = baseUrl + separator + 'nextAction=' + encodeURIComponent(nextAction) + '&tokenId=' + encodeURIComponent(token);
   }
-  
-  // Check if URL already has query parameters
-  var separator = baseUrl.indexOf('?') !== -1 ? '&' : '?';
-  
-  // Build full URL with parameters
-  fullUrl = baseUrl + separator + 'nextAction=' + encodeURIComponent(nextAction) + '&tokenId=' + encodeURIComponent(token);
   
   // Try to open the web URL
   window.location.href = fullUrl;
