98e566114672d5fefe4f9bd18811db1f518f0282
Ammales Yamsompong <ammales.y@kbtg.tech>
2025-11-19 16:47:09 +0700

    Update UAT url

--- kplus-deeplink.js
+++ kplus-deeplink.js
@@ -6,9 +6,11 @@
 
 function KPlusDeepLinkHandler() {
   this.huaweiUrl = "https://kplusuat.dra.agconnect.link/?deeplink=https%3A%2F%2Fwww.kasikornbank.com%2Fth%2Fkplus%2Fdeeplinkkplus&android_deeplink=kbank.kplus%3A%2F%2Fauthenwithkplus%3FnextAction%3DNEXT_ACTION_REPLACEMENT%26tokenId%3DTOKEN_ID_REPLACEMENT&android_fallback_url=https%3A%2F%2Fwww.kasikornbank.com%2Fth%2Fkplus%2Fdeeplinkkplus&android_open_type=3&android_package_name=com.kasikorn.retail.mbanking.wap2&campaign_channel=First+Test+HMS&harmonyos_deeplink=kbank.kplus%3A%2F%2Fauthenwithkplus&preview_type=2&landing_page_type=2&region_id=3";
-  this.generalUrl = "https://www.kasikornbank.com/th/kplus/deeplinkkplus/";
+  // this.generalUrl = "https://www.kasikornbank.com/th/kplus/deeplinkkplus/";
+  this.generalUrl = "https://kbank-uat.kasikornbank.com/th/kplus/deeplinkkplus/";
   this.fallbackDuration = 3000; // Duration to wait before fallback (in milliseconds)
-  this.fallbackUrl = 'https://www.kasikornbank.com/th/kplus/deeplinkkplus/';
+  // this.fallbackUrl = 'https://www.kasikornbank.com/th/kplus/deeplinkkplus/';
+  this.fallbackUrl = 'https://kbank-uat.kasikornbank.com/th/kplus/deeplinkkplus/';
   this.scheme = 'kbank.kplus://';
 
   // Detect WebView/in-app browser using reliable indicators
